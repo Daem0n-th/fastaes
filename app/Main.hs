81a963121cfb2ecf58235d5c767ea6fc8de8b946
Daem0n-th <dheemanthmanur72@gmail.com>
2021-11-23 22:24:01 +0530

    Add sub commands gen, enc and dec

--- app/Main.hs
+++ app/Main.hs
@@ -6,33 +6,53 @@ import Crypto.Aes (decryptFile, encryptFile)
 import Data.ByteString (ByteString)
 import System.Console.CmdArgs
 
-data AesArgs = AesArgs
-  { output :: FilePath,
-    decrypt :: Bool,
-    key :: FilePath,
-    input :: FilePath
-  }
+data AesArgs
+  = Gen {output :: FilePath}
+  | Enc
+      { output :: FilePath,
+        key :: FilePath,
+        input :: FilePath
+      }
+  | Dec
+      { output :: FilePath,
+        key :: FilePath,
+        input :: FilePath
+      }
   deriving (Data, Typeable, Show, Eq)
 
-aesArgs :: AesArgs
-aesArgs =
-  AesArgs
-    { output = def &= name "o" &= help "Filename for the output data.",
-      decrypt = def &= name "d" &= help "Decrypt the file.",
-      key = def &= name "k" &= help "File containing the AES key to encrypt/decrypt the file.",
-      input = def &= args &= typ "INPUT FILE"
+gen :: AesArgs
+gen =
+  Gen
+    { output = def &= argPos 0 &= typFile
     }
-    &= verbosity
-    &= help "Encrypt/Decrypt files in AES256"
-    &= summary "fastaes v0.1.0.0, (C) Dheemanth Manur"
-    &= details
-      [ "Hlint gives hints on how to improve Haskell code",
-        "",
-        "To check all Haskell files in 'src' and generate a report type:",
-        "  hlint src --report"
-      ]
+
+enc :: AesArgs
+enc =
+  Enc
+    { output = def &= name "o" &= help "File name for the encrypted data.",
+      key = def &= name "k" &= help "AES key file to encrypt the file with.",
+      input = def &= argPos 1 &= typFile
+    }
+
+dec :: AesArgs
+dec =
+  Dec
+    { output = def &= name "o" &= help "File name for the decrypted data.",
+      key = def &= name "k" &= help "AES key file to decrypt the file with.",
+      input = def &= argPos 2 &= typFile
+    }
+
+mode :: Mode (CmdArgs AesArgs)
+mode =
+  cmdArgsMode $
+    modes [gen, enc, dec]
+      &= help "Generate AES keys or Encrypt/Decrypt files in AES256"
+      &= program "fastaes"
+      &= summary "fastaes v0.1.0.0, (C) Dheemanth Manur"
 
 main :: IO ()
-main = handleArgs =<< cmdArgs aesArgs
+main = handleArgs =<< cmdArgsRun mode
   where
-    handleArgs (AesArgs outF decrypt k inF) = if decrypt then decryptFile k inF outF else encryptFile k inF outF
+    handleArgs (Gen outF) = undefined
+    handleArgs (Enc outF keyF inF) = encryptFile keyF inF outF
+    handleArgs (Dec outF keyF inF) = decryptFile keyF inF outF
